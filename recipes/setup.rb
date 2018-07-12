a62fae6a02724dc21f8b448424ac47f74c78039b
Duggy <dugekeb1@gmail.com>
2018-07-12 22:29:59 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,24 @@
+package 'vim-enhanced'
+package 'nano'
+
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp' 
+
+file '/etc/motd' do
+  content 'This computer is a property of mine'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+action [ :enable, :start ]
+end
+
